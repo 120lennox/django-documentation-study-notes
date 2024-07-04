3018f38b988d2799cf2d4e05f0afa2a59fd41929
Lennox Mountain <lennoxmountain67@gmail.com>
2024-07-04 17:11:01 +0200

    added database fields

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField( max_length=50)
+    members = models.ManyToManyField(Person, through="Membership")
+
+    def __str__(self) -> str:
+        return self.name
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateField()
+    invite_reason = models.TextField(max_length=60)
+
